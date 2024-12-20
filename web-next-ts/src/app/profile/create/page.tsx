31a8fe42dcf545a868f6a0fc4b65ef8226e70a1c
John-Dag Addy <jaedagy@gmail.com>
2024-12-20 10:28:42 +0000

    refactor: streamline default values 1

--- web-next-ts/src/app/profile/create/page.tsx
+++ web-next-ts/src/app/profile/create/page.tsx
@@ -21,7 +21,16 @@ export default function CreateProfilePage() {
   const router = useRouter()
 
   const defaultValues: CreateProfileFormData = {
-    firstName: '', lastName: '', email: '', address: '', city: '', country: '', county: '', gender: '', phone: '', state: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    city: '',
+    country: '',
+    county: '',
+    gender: '',
+    phone: '',
+    state: '',
     zipPostal: '',
   }
 
