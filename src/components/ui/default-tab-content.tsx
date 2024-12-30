6837fa526d45a70316363571aa884afcf85a24c8
John-Dag Addy <jaedagy@gmail.com>
2024-12-30 12:59:27 +0000

    chore: uncomment content code

--- src/components/ui/default-tab-content.tsx
+++ src/components/ui/default-tab-content.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Flex, Text } from '@chakra-ui/react'
+import { Center, Flex, Text } from '@chakra-ui/react'
 import HandIcon from '../icons/HandIcon'
 
 function DefaultTabContent() {
