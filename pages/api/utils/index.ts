635e835bada5113832499c5723283339a80a1bce
John-Dag Addy <jaedagy@gmail.com>
2025-01-06 00:53:24 +0000

    feat: enhance generatePersonBio function to include additional personal details

--- pages/api/utils/index.ts
+++ pages/api/utils/index.ts
@@ -1,5 +1,30 @@
 import { Person } from '@/gql/graphql'
-
 export const generatePersonBio = (person: Person) => {
-  return `${person.firstName} ${person.lastName} prefers the pronouns ${person.pronouns}. ${person.firstName} phone number is ${person.phone} and email is ${person.email}. ${person.firstName} lives in ${person.location}.`
+  const pronouns = person.pronouns
+    ? `prefers the pronouns ${person.pronouns}.`
+    : ''
+  const phone = person.phone
+    ? `${person.firstName}'s phone number is ${person.phone}.`
+    : ''
+  const location = person.location
+    ? `${person.firstName} lives in ${person.location}.`
+    : ''
+
+  const favorites = person.favorites
+    ? `${person.firstName} enjoys ${person.favorites}.`
+    : ''
+  const passions = person.passions
+    ? `${person.firstName} is passionate about ${person.passions}.`
+    : ''
+  const traits = person.traits
+    ? `${person.firstName} is known for being ${person.traits}.`
+    : ''
+  const fieldsOfCare = person.fieldsOfCare
+    ? `${person.firstName} cares about ${person.fieldsOfCare}.`
+    : ''
+  const interests = person.interests
+    ? `${person.firstName} is interested in ${person.interests}.`
+    : ''
+
+  return `${person.firstName} ${person.lastName} ${pronouns} ${phone} ${person.firstName}'s email is ${person.email}. ${location} ${favorites} ${passions} ${traits} ${fieldsOfCare} ${interests}`.trim()
 }
