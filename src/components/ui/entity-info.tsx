a690b7454476b6307ca1896a9854003cc69539c2
John-Dag Addy <jaedagy@gmail.com>
2025-01-09 23:44:18 +0000

    fix: overlowing and misaligned text on entity information display

--- src/components/ui/entity-info.tsx
+++ src/components/ui/entity-info.tsx
@@ -5,15 +5,15 @@ export function EntityInfo({ entity }: { entity: any }) {
     return Object.entries(entity).map(([key, value]) => (
       <Flex
         key={key}
-        justifyContent="space-between"
         width="100%"
         flexDirection={{ base: 'column', md: 'row' }}
         gap={2}
+        alignItems="flex-start"
       >
-        <Text fontWeight="bold" fontSize="xs">
+        <Text fontWeight="bold" fontSize="xs" width="50%">
           {key.toUpperCase()}:
         </Text>{' '}
-        <Text fontSize="sm" textAlign="start">
+        <Text fontSize="sm" textAlign="start" width="50%">
           {value?.toString()}
         </Text>
       </Flex>
