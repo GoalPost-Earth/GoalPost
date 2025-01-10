cb0fd8480cade6ab36ece15762fb9b457469f3fc
John-Dag Addy <jaedagy@gmail.com>
2025-01-10 00:01:55 +0000

    chore: tweak space allocation for texts

--- src/components/ui/entity-info.tsx
+++ src/components/ui/entity-info.tsx
@@ -7,13 +7,13 @@ export function EntityInfo({ entity }: { entity: any }) {
         key={key}
         width="100%"
         flexDirection={{ base: 'column', md: 'row' }}
-        gap={2}
-        alignItems="flex-start"
+        gap={1}
+        justifyContent="flex-start"
       >
-        <Text fontWeight="bold" fontSize="xs" width="50%">
+        <Text fontWeight="bold" fontSize="xs" width="40%">
           {key.toUpperCase()}:
         </Text>{' '}
-        <Text fontSize="sm" textAlign="start" width="50%">
+        <Text fontSize="sm" textAlign="start" width="60%">
           {value?.toString()}
         </Text>
       </Flex>
