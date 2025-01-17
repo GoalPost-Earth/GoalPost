f71323fd72387d6d6ef5c924bb3a225af571fded
John-Dag Addy <jaedagy@gmail.com>
2025-01-17 02:22:07 +0000

    feat: show default tab page if data does not exist

--- src/components/sections/goal/cared-for-by-carepoints.tsx
+++ src/components/sections/goal/cared-for-by-carepoints.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { CarePointCard } from '@/components'
+import DefaultTabContent from '@/components/ui/default-tab-content'
 import { Goal } from '@/gql/graphql'
 import { Grid, GridItem } from '@chakra-ui/react'
 import React from 'react'
 
 export default function GoalCaredForByCarePoints({ goal }: { goal: Goal }) {
-  return (
+  return goal.caredForByCarePoints.length > 0 ? (
     <Grid templateColumns="repeat(auto-fill, minmax(360px, 1fr))" gap={6}>
       {goal.caredForByCarePoints.map((carePoint) => (
         <GridItem key={carePoint.id}>
@@ -14,5 +15,7 @@ export default function GoalCaredForByCarePoints({ goal }: { goal: Goal }) {
         </GridItem>
       ))}
     </Grid>
+  ) : (
+    <DefaultTabContent />
   )
 }
